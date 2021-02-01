a2cd3c8d9009e67d69bd11d3c4e172056fd4f1fd
serius ubuntu <seriusman0@gmail.com>
2021-02-01 08:18:55 +0700

    Class

--- 1. Object/script.js
+++ 1. Object/script.js
@@ -43,24 +43,72 @@
 // let serius = new Mahasiswa("Seriusman Waruwu", 10);
 
 
-const methodMahasiswa = {
-    makan: function(porsi){
-        this.energi += porsi;
-    },
-    main: function(jam){
-        this.energi-=jam;
-    },
-    tidur: function(jam){
-        this.energi+= jam*2;
-    }
-};
+// const methodMahasiswa = {
+//     makan: function(porsi){
+//         this.energi += porsi;
+//     },
+//     main: function(jam){
+//         this.energi-=jam;
+//     },
+//     tidur: function(jam){
+//         this.energi+= jam*2;
+//     }
+// };
 
-function Mahasiswa(nama, energi){
-    mahasiswa = Object.create(methodMahasiswa);
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
+// function Mahasiswa(nama, energi){
+//     mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
     
-    return mahasiswa;
-}
+//     return mahasiswa;
+// }
 
-let serius = Mahasiswa("Seriusman Waruwu", 10);
+// let serius = Mahasiswa("Seriusman Waruwu", 10);
+
+
+// PROTOTYPE
+// function Mahasiswa (nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+// }  
+
+// Mahasiswa.prototype.makan = function(porsi){
+//     this.energi += porsi;
+//     return `Hai ${this.nama}, Selamat Makan`;
+// }
+
+// Mahasiswa.prototype.main = function(jam){
+//     this.energi -= jam;
+//     return `Energi ${this.nama} berkurang hingga ${this.energi}`;
+// }
+
+// Mahasiswa.prototype.tidur = function(jam) {
+//     this.energi += jam*2;
+//     return `Energi ${this.nama} bertambah menjadi ${this.energi}`;
+// }
+
+// let serius = new Mahasiswa("Seriusman Waruwu", 10);
+
+
+//CLASS
+class Mahasiswa {
+    constructor (nama, energi) {
+        this.nama = nama;
+        this.energi = energi;
+    }
+
+    makan (porsi){
+        this.energi += porsi;
+        return `Selamat Makan ${this.nama}, energi = ${this.energi}`;
+    }
+
+    main (jam){
+        this.energi -= jam;
+        return `Selamat Main ${this.nama}, energi = ${this.energi}`;
+    }
+
+    tidur(jam){
+        this.energi += jam*2;
+        return `Selamat Tidur ${this.nama}, energi = ${this.energi}`;
+    }
+}
